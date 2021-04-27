1d79b5e1fb718a83ce39cd84993a165e3ec74803
Jesus Cordova <jesuscordova.mail.weber.edu>
2021-04-26 19:53:43 -0700

    project submission

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
-import React from "react";
-import MainPage from "./components/MainPage.js";
-import Blog from "./components/Blog.js";
-import Article from "./components/Article.js";
-import Authentication from "./components/AuthenticationPage/Authentication.js";
-import Navigation from "./components/Navigation.js";
-import YourComments from "./components/YourComments.js";
-import Settings from "./components/Settings.js";
-import { UserProvider } from "./Context/UserContext.js";
-import { ArticleProvider } from "./Context/ArticleContext.js";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import UnblockMe from "./UnblockMe.js";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <UserProvider>
-          <ArticleProvider>
-            <Navigation />
-            <Switch>
-              <Route path="/" exact component={MainPage} />
-            </Switch>
-            <Switch>
-              <Route path="/Blog" component={Blog} />
-            </Switch>
-            <Switch>
-              <Route path="/Article/" component={Article} />
-            </Switch>
-            <Switch>
-              <Route path="/Authentication" component={Authentication} />
-            </Switch>
-            <Switch>
-              <Route path="/Comments" component={YourComments} />
-            </Switch>
-            <Switch>
-              <Route path="/Settings" component={Settings} />
-            </Switch>
-          </ArticleProvider>
-        </UserProvider>
-      </Router>
+      <UnblockMe> </UnblockMe>
     </div>
   );
 }
